23d1a34860113f304573cbae1476bbaa2793ebc9
Roman Kutselepa <roman.kutselepa@gmail.com>
2023-10-31 15:39:37 +0200

    Latest

--- 012_oop/homework/main.py
+++ 012_oop/homework/main.py
@@ -1,33 +1,36 @@
+# Your classes go here
 
 
-if __name__ == "__main__":
-    zoo = Zoo()
 
-    lion = Mammal('Simba', 'Lion', 'Golden')
-    eagle = Bird('Baldy', 'Eagle', 100)
-    python = Reptile('Monty', 'Python', 'Diamond')
-    royal_python = Reptile('Muscle', 'Python', 'Triangle')
-
-    zoo.add_animal(lion)
-    zoo.add_animal(eagle)
-    zoo.add_animal(python)
-    zoo.add_animal(royal_python)
-
-    print('Zoo animals:')
-    zoo.list_animals()
-
-    print('Feeding animals:')
-    zoo.feed_animals()
-
-    print('Removing animal:')
-    zoo.remove_animal("Baldy")
-
-    print('Zoon animals after removing:')
-    zoo.list_animals()
-
-    print('Total number of animals', zoo.count_animals())
-
-    print('List all reptiles:')
-    reptiles = zoo.get_animals_by_species('Python')
-    for reptile in reptiles:
-        print(f'{reptile.get_name()} ({reptile.get_species()})')
+# # You can use code below for testing purpouses if you want
+# if __name__ == "__main__":
+#     zoo = Zoo()
+#
+#     lion = Mammal('Simba', 'Lion', 'Golden')
+#     eagle = Bird('Baldy', 'Eagle', 100)
+#     python = Reptile('Monty', 'Python', 'Diamond')
+#     royal_python = Reptile('Muscle', 'Python', 'Triangle')
+#
+#     zoo.add_animal(lion)
+#     zoo.add_animal(eagle)
+#     zoo.add_animal(python)
+#     zoo.add_animal(royal_python)
+#
+#     print('Zoo animals:')
+#     zoo.list_animals()
+#
+#     print('Feeding animals:')
+#     zoo.feed_animals()
+#
+#     print('Removing animal:')
+#     zoo.remove_animal("Baldy")
+#
+#     print('Zoon animals after removing:')
+#     zoo.list_animals()
+#
+#     print('Total number of animals', zoo.count_animals())
+#
+#     print('List all reptiles:')
+#     reptiles = zoo.get_animals_by_species('Python')
+#     for reptile in reptiles:
+#         print(f'{reptile.get_name()} ({reptile.get_species()})')
